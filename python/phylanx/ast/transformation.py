acbd9cb7331b2030d9203da1ccebef501b629dc1
ct-clmsn <ct-clmsn(at)gmail>
2018-06-24 14:55:02 -0400

    mods to appease circleci

--- python/phylanx/ast/transformation.py
+++ python/phylanx/ast/transformation.py
@@ -424,8 +424,8 @@ class PhySL:
         elif nn == "Div":
             sym = "/"
         else:
-            le_str = "Undefined operation discovered: %s=" % (nn,)
-            raise LookupError( le_str )
+            raise LookupError( ("Undefined operation",
+                " discovered: %s=" % (nn,) ) )
 
         id_ = "UNKNOWN_ID"
         if type(args[0]) == ast.Subscript:
